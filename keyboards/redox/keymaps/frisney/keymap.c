537b8d2a5050d89055b065fb73973a03345d2ae9
Felipe Risney <41243324+FRisney@users.noreply.github.com>
2024-12-19 16:39:20 -0300

    add workman layout as default and qwerty as secondary

--- keyboards/redox/keymaps/frisney/keymap.c
+++ keyboards/redox/keymaps/frisney/keymap.c
@@ -1,20 +1,37 @@
 #include QMK_KEYBOARD_H
 
-#define _BASE 0
-#define _LOWR 1
-#define _UPPR 2
-#define _ADJU 3
-#define _NAVI 4
+#define _WRKM 0
+#define _QWERT 1
+#define _LOWR 2
+#define _UPPR 3
+#define _ADJU 4
+#define _NAVI 5
 
 #define TGNAVI TG(_NAVI)
+#define TGQWRT TG(_QWERT)
+#define TGWRKM TG(_WRKM)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_BASE] = LAYOUT(
+  [_WRKM] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,TGNAVI  ,                          TGNAVI  ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSPC ,
+     KC_TAB  ,KC_Q    ,KC_E    ,KC_R    ,KC_W    ,KC_B    ,TGNAVI  ,                          TGQWRT  ,KC_J    ,KC_F    ,KC_U    ,KC_P    ,KC_SCLN ,KC_BSPC ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┤────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_ESC  ,KC_A    ,KC_S    ,KC_H    ,KC_T    ,KC_G    ,KC_DEL  ,                          KC_INS  ,KC_Y    ,KC_N    ,KC_E    ,KC_O    ,KC_I    ,KC_QUOT ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┤────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT ,KC_Z    ,KC_X    ,KC_M    ,KC_C    ,KC_V    ,KC_PGUP ,KC_PGDN ,        KC_LALT ,KC_RCTL ,KC_K    ,KC_L    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
+  //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
+     KC_LGUI ,KC_PPLS ,KC_MINS ,KC_LALT ,     TL_LOWR ,    KC_LCTL ,KC_SPACE,        KC_ENTER,KC_LGUI ,    TL_UPPR ,     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT
+  //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
+  ),
+
+  [_QWERT] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_GRV  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                            KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,TGNAVI  ,                          TGWRKM  ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSPC ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┤────────┼────────┼────────┼────────┼────────┼────────┤
      KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_DEL  ,                          KC_INS  ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┤────────┼────────┼────────┼────────┼────────┼────────┤
